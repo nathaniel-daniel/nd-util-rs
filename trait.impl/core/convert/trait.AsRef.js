86a9087df4d5770e81577fa11bc909de64efa9a8
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-07-02 08:10:06 +0000

    deploy: e32a7e167218e1dae40651f13e8e40cab56c26fb

---
+++ trait.impl/core/convert/trait.AsRef.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"nd_util":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.79.0/std/path/struct.Path.html\" title=\"struct std::path::Path\">Path</a>&gt; for <a class=\"struct\" href=\"nd_util/struct.DropRemovePath.html\" title=\"struct nd_util::DropRemovePath\">DropRemovePath</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/core/fmt/trait.Debug.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"nd_util":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"nd_util/struct.ArcAnyhowError.html\" title=\"struct nd_util::ArcAnyhowError\">ArcAnyhowError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"nd_util/struct.DropRemovePath.html\" title=\"struct nd_util::DropRemovePath\">DropRemovePath</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
