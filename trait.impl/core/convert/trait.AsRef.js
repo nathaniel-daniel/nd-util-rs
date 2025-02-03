f7011206d4b19216ad8777a664f124279f0b100b
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-02-03 21:54:39 +0000

    deploy: bc845ab412ac2a41c9ea1b9cf10735d0061a573f

---
+++ trait.impl/core/convert/trait.AsRef.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["nd_util",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.84.0/std/path/struct.Path.html\" title=\"struct std::path::Path\">Path</a>&gt; for <a class=\"struct\" href=\"nd_util/struct.DropRemovePath.html\" title=\"struct nd_util::DropRemovePath\">DropRemovePath</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[425]}
---
+++ trait.impl/core/panic/unwind_safe/trait.UnwindSafe.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["nd_util",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/panic/unwind_safe/trait.UnwindSafe.html\" title=\"trait core::panic::unwind_safe::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"nd_util/struct.ArcAnyhowError.html\" title=\"struct nd_util::ArcAnyhowError\">ArcAnyhowError</a>",1,["nd_util::arc_anyhow_error::ArcAnyhowError"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/panic/unwind_safe/trait.UnwindSafe.html\" title=\"trait core::panic::unwind_safe::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"nd_util/struct.DropRemovePath.html\" title=\"struct nd_util::DropRemovePath\">DropRemovePath</a>",1,["nd_util::drop_remove_path::DropRemovePath"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[731]}
