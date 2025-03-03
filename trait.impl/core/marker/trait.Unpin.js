65af1e7af00678a3fd1c6c55cfb0626b5020a6c4
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-03-03 02:46:38 +0000

    deploy: 9fdf68ea3f07f912a77e0ad474469c4f342cfbf0

---
+++ trait.impl/core/marker/trait.Unpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["nd_util",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"nd_util/struct.ArcAnyhowError.html\" title=\"struct nd_util::ArcAnyhowError\">ArcAnyhowError</a>",1,["nd_util::arc_anyhow_error::ArcAnyhowError"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"nd_util/struct.DropRemovePath.html\" title=\"struct nd_util::DropRemovePath\">DropRemovePath</a>",1,["nd_util::drop_remove_path::DropRemovePath"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[655]}
